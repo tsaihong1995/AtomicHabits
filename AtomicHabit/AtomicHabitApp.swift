926f85154ec3cd813945dc4d94fe552dbe46ee4f
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-02-08 21:07:30 -0500

    Initial Commit

---
+++ AtomicHabit/AtomicHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomicHabitApp.swift
+//  AtomicHabit
+//
+//  Created by Hung-Chun Tsai on 2021-02-08.
+//
+
+import SwiftUI
+
+@main
+struct AtomicHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
